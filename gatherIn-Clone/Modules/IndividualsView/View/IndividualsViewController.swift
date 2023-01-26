a22fe2a33db429fc481f43c5f91d22115808214c
MahmoudHDev <Mahmoud.ios.dev@gmail.com>
2023-01-26 12:15:21 +0200

    building the Individuals view

---
+++ gatherIn-Clone/Modules/IndividualsView/View/IndividualsViewController.swift
@@ -0,0 +1,46 @@
+//
+//  IndividualsViewController.swift
+//  gatherIn-Clone
+//
+//  Created by Mahmoud Hashim on 1/26/23.
+//
+
+import UIKit
+
+class IndividualsViewController: UIViewController {
+    //MARK:- Outlets
+    @IBOutlet weak var welcomeLbl               : UILabel!
+    @IBOutlet weak var PhoneNumberIntroLbl      : UILabel!
+    @IBOutlet weak var phoneNumberLbl           : UILabel!
+    
+    @IBOutlet weak var countryCodeContainerView : UIView!
+    @IBOutlet weak var countrycodeImg           : UIImage!
+    @IBOutlet weak var CountryCodeBtn           : UIButton!
+    @IBOutlet weak var chevonorArrowImg         : UIImage!
+    
+    @IBOutlet weak var phoneNumberContainerView : UIView!
+    @IBOutlet weak var phoneNumberTextField     : UITextField!
+    
+    @IBOutlet weak var errorMessageLbl          : UILabel!
+    @IBOutlet weak var loginBtn                 : UIButton!
+    @IBOutlet weak var confirmationMessageLbl   : UILabel!
+    @IBOutlet weak var noteMessageLbl           : UILabel!
+    
+    
+    // MARK:- Properties
+    
+    // MARK:- View life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    // MARK:- Methods
+    
+    // MARK:- Actions
+    @IBAction func loginBtn(_ sender: UIButton) {
+        
+    }
+    
+}
