f766b15f2a118dc886a45ff85b0e1f41bf431de5
MahmoudHDev <Mahmoud.ios.dev@gmail.com>
2023-01-26 11:04:20 +0200

    buttons one for individuals and the other for business

--- gatherIn-Clone/Modules/MainView/View/MainViewController.swift
+++ gatherIn-Clone/Modules/MainView/View/MainViewController.swift
@@ -10,18 +10,38 @@ import UIKit
 class MainViewController: UIViewController {
     
     // MARK:- Outlets
-    
+    @IBOutlet weak var individualsBtn   :UIButton!
+    @IBOutlet weak var businessBtn      :UIButton!
     // MARK:- Properties
     var presenter: MainPresenter?
     // MARK:- View life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         presenter = MainPresenter(view: self)
-        // Do any additional setup after loading the view.
+        self.styleUI()
+        
     }
     
     // MARK:- Methods
+    private func styleUI() {
+        self.navigationController?.navigationBar.isHidden = true
+        
+        self.individualsBtn.layer.cornerRadius = 20
+        self.businessBtn.layer.cornerRadius = 20
+        self.individualsBtn.layer.masksToBounds = false
+        self.businessBtn.layer.masksToBounds = false
+    }
     
+    // MARK:- Actions
+    @IBAction func forIndividutualBtn(_ sender: UIButton) {
+        print("Individuals' view")
+        // self.push(//)
+    }
+    
+    @IBAction func forBusinessBtn(_ sender: UIButton) {
+        print("Business' view")
+        
+    }
 }
 
 // MARK:- Presenter
