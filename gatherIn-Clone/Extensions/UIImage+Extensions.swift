ae11b852e0324b2e567965f1de9a894d8c428b68
MahmoudHDev <Mahmoud.ios.dev@gmail.com>
2023-03-19 14:12:30 +0200

    add some Files

---
+++ gatherIn-Clone/Extensions/UIImage+Extensions.swift
@@ -0,0 +1,21 @@
+//
+//  UIImage+Extensions.swift
+//  gatherIn-Clone
+//
+//  Created by Mahmoud Hashim on 3/17/23.
+//
+
+import UIKit.UIImage
+extension UIImage {
+    func resize(to newSize: CGSize) -> UIImage {
+        
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 0.0)
+        draw(in: CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height))
+        
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        
+        return newImage
+    }
+}
+// 
