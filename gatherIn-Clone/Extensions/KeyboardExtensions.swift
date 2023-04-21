8182f9138a03b5a7970a6230165b6324608acb90
Mahmoud Hashim <Mahmoud.ios.dev@Gmail.com>
2023-04-21 16:52:50 +0200

    adding some style to the views

--- gatherIn-Clone/Extensions/KeyboardExtensions.swift
+++ gatherIn-Clone/Extensions/KeyboardExtensions.swift
@@ -5,17 +5,14 @@
 //  Created by Mahmoud Hashim on 2/3/23.
 //
 
-import UIKit
+import UIKit.UIView
 
 
 extension UIViewController {
-// Dismiss keyboard in all UIViewControllers
     func hideKeyboardWhenTappedAround() {
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
         view.addGestureRecognizer(tapGesture)
-        
     }
-    
     @objc
     func hideKeyboard() {
         view.endEditing(true)
